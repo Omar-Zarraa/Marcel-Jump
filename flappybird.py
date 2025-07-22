33ba44c1149edf673b2cff650c76aa751360d81b
Omar Zarraa <omarzarraa@proton.me>
2025-07-22 19:03:54 +0300

    Hitboxes created for Pole and Character classes

--- flappybird.py
+++ flappybird.py
@@ -6,18 +6,22 @@ time = 0
 
 
 class Character:
-    def __init__(self, image: str, x: float, y: float, w: float=200, h: float=200) -> None:
+    def __init__(
+        self, image: str, x: float, y: float, w: float = 200, h: float = 200
+    ) -> None:
         self.image = pygame.image.load(image).convert_alpha()
         self.x = x
         self.y = y
-        self.pos=(x,y)
+        self.pos = (x, y)
+        self.hitBox = (w, h)
         self.image = pygame.transform.scale(self.image, (w, h))
 
     def draw(self, screen: pygame.Surface):
+        pygame.draw.rect(screen, pygame.Color("black"), self.hitBox)
         screen.blit(self.image, (self.x, self.y))
-        
+
     @property
-    def x(self):
+    def x(self) -> float:
         return self._x
 
     @x.setter
@@ -25,35 +29,44 @@ class Character:
         self._x = x
 
     @property
-    def y(self):
+    def y(self) -> float:
         return self._y
 
     @y.setter
     def y(self, y: float):
         self._y = y
-        
+
     @property
-    def pos(self):
+    def pos(self) -> tuple:
         return self._pos
 
     @pos.setter
     def pos(self, pos: tuple):
         self._pos = pos
 
+    @property
+    def hitBox(self) -> pygame.Rect:
+        return self._hitBox
+
+    @hitBox.setter
+    def hitBox(self, widthHeight: tuple):
+        self._hitBox = pygame.Rect(self.pos, widthHeight)
+
 
 class Pole:
-    def __init__(self,image:str, x: float, y: float, w: float, h: float) -> None:
+    def __init__(self, image: str, x: float, y: float, w: float, h: float) -> None:
         self.image = pygame.image.load(image).convert_alpha()
         self.x = x
         self.y = y
         self.pos = (x, y)
+        self.hitBox = (w, h)
         self.image = pygame.transform.scale(self.image, (w, h))
 
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
     @property
-    def x(self):
+    def x(self) -> float:
         return self._x
 
     @x.setter
@@ -61,7 +74,7 @@ class Pole:
         self._x = x
 
     @property
-    def y(self):
+    def y(self) -> float:
         return self._y
 
     @y.setter
@@ -69,15 +82,43 @@ class Pole:
         self._y = y
 
     @property
-    def pos(self):
+    def pos(self) -> tuple:
         return self._pos
 
     @pos.setter
     def pos(self, pos: tuple):
         self._pos = pos
-        
+
+    @property
+    def hitBox(self) -> pygame.Rect:
+        return self._hitBox
+
+    @hitBox.setter
+    def hitBox(self, widthHeight: tuple):
+        self._hitBox = pygame.Rect(self.pos, widthHeight)
+
+
 def areColliding(character: Character, pole: Pole) -> bool:
-    return character.pos==pole.pos
+    return character.pos == pole.pos
+
+
+def characterGravity(player: Character, screen: pygame.Surface) -> bool:
+    player.draw(screen)
+    global time
+    time += 1
+    if player.y < screen.get_height() - 200:
+        global speed
+        speed = gravity * time / 60
+        player.y = player.y + speed
+    else:
+        speed = 1
+        time = 0
+
+    if player.y > screen.get_height() - 201:
+        print("Dead")
+        return False
+
+    return True
 
 
 def main():
@@ -87,7 +128,7 @@ def main():
     clock = pygame.time.Clock()
     font = pygame.font.SysFont("font", 20)
     player = Character("character.png", 540, 260, 150, 150)
-    pole = Pole("Pole.png",550,200,300,600)
+    pole = Pole("Pole.png", 550, 200, 300, 600)
     running = True
 
     while running:
@@ -113,22 +154,11 @@ def main():
         screen.fill("white")
 
         # RENDER YOUR GAME HERE
-        player.draw(screen)
+        if not characterGravity(player, screen):
+            running = False
         pole.draw(screen)
         # pole.x -= 10
-        print(areColliding(player,pole))
-        time += 1
-        if player.y < screen.get_height() - 200:
-            global speed
-            speed = gravity * time / 60
-            player.y = player.y + speed
-        else:
-            speed = 1
-            time = 0
-
-        if player.y > screen.get_height() - 201:
-            print("Dead")
-            running = False
+        print(areColliding(player, pole))
 
         pygame.display.update()
         pygame.display.flip()
